67e58a92f61ed2348e4dcbb0e658f33b358ee50f
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-04-30 22:28:41 +0530

    Confetti and sound effect added.Displayed Gift Name

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 document.getElementById("btn").addEventListener("click", () => {
 const value =(Math.floor(Math.random()*10+1))
 document.getElementById("box").innerHTML=value;
+const jsconfetti=new JSConfetti();
+const sound=new Audio("celebration.mp3");
+jsconfetti.addConfetti()
+sound.play()
+sound.currentTime=0;
+const prizes = [
+  "₹5 Discount!!!","Better Luck Next Time!!!","₹10 Discount!!!","₹3 Discount!!!","Better Luck Next Time!!!",
+  "Special Prize 🎁🎁!!!","₹2 Discount!!!","Better Luck Next Time!!!","₹8 Discount!!!","₹1 Discount!!!",
+  "Better Luck Next Time!!!","₹12 Discount!!!","₹4 Discount!!!","Better Luck Next Time!!!","₹6 Discount!!!"
+];const display=prizes[value];
+console.log(display)
+document.getElementById("text-suprise").innerHTML=display;
 });
