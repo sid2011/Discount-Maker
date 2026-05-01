cfa44b15c693986eec4c2862b10401eb7d476af2
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-05-01 09:31:31 +0530

    Removed unneccesary array items

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").addEventListener("click", () => {
-const value =(Math.floor(Math.random()*10+1))
+const value =(Math.floor(Math.random()*10))
 document.getElementById("box").innerHTML=value;
 const jsconfetti=new JSConfetti();
 const sound=new Audio("celebration.mp3");
@@ -9,7 +9,6 @@ sound.currentTime=0;
 const prizes = [
   "₹5 Discount!!!","Better Luck Next Time!!!","₹10 Discount!!!","₹3 Discount!!!","Better Luck Next Time!!!",
   "Special Prize 🎁🎁!!!","₹2 Discount!!!","Better Luck Next Time!!!","₹8 Discount!!!","₹1 Discount!!!",
-  "Better Luck Next Time!!!","₹12 Discount!!!","₹4 Discount!!!","Better Luck Next Time!!!","₹6 Discount!!!"
 ];const display=prizes[value];
 console.log(display)
 document.getElementById("text-suprise").innerHTML=display;
