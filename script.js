402e536bbf0d37f33af383d48926f315dbe429c8
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-05-01 10:52:42 +0530

    Fixed issues

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //optimised code
-//Number show
+//Number show & Prizes Display
 const button=document.getElementById("btn");
 button.addEventListener("click", () => {
 const value =(Math.floor(Math.random()*10))
-const box= document.getElementById("box").innerHTML=value;
+const box= document.getElementById("text-box").innerHTML=value;
+button.addEventListener("click",()=>{
+  const prizes = [
+  "₹5 Discount!!!","Better Luck Next Time!!!","₹10 Discount!!!","₹3 Discount!!!","Better Luck Next Time!!!",
+  "Special Prize 🎁🎁!!!","₹2 Discount!!!","Better Luck Next Time!!!","₹8 Discount!!!","₹1 Discount!!!",
+];const display=prizes[value];
+console.log(display)
+document.getElementById("text-suprise").innerHTML=display;
+})
 });
 //Audio and Confetti animations
 button.addEventListener("click", () => {
@@ -13,13 +21,4 @@ jsconfetti.addConfetti()
 sound.play()
 sound.currentTime=0;
 });
-//Prizes Display
-button.addEventListener("click",()=>{
-  const prizes = [
-  "₹5 Discount!!!","Better Luck Next Time!!!","₹10 Discount!!!","₹3 Discount!!!","Better Luck Next Time!!!",
-  "Special Prize 🎁🎁!!!","₹2 Discount!!!","Better Luck Next Time!!!","₹8 Discount!!!","₹1 Discount!!!",
-];const display=prizes[value];
-console.log(display)
-document.getElementById("text-suprise").innerHTML=display;
-})
 
